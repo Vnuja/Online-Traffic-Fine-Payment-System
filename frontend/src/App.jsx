bf2428f423e8b19b963bc62f30c947e1521d2b9f
Aqeel Nazar <aqeelnazar212@gmail.com>
2025-03-18 22:20:29 +0530

    Admin bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,24 @@
-<<<<<<< HEAD
 import React from "react";
 import AdminRegister from "./Components/Admin/AdminRegister"; 
-=======
 import { Route, Routes } from "react-router-dom";
 
 //User Management System
 import SignUp from "./components/User/SignUp";
 import Login from "./components/User/Login";
->>>>>>> e3f291480ce07b38357668e5cce8132c21161d70
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="app-container">
-      <AdminRegister /> {}
-    </div>
-  );
-=======
     <div>
       <Routes>
+        {/* Admin */}
+        <Route path="/admin" element={<AdminRegister />} />
+        
+        {/* User */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
->>>>>>> e3f291480ce07b38357668e5cce8132c21161d70
 }
 
 export default App;
