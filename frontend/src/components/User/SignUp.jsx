81b333e1c74a90f51bc277aed5e9099d6736bee4
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2025-03-19 22:30:47 +0530

    Change signup pages

--- frontend/src/components/User/SignUp.jsx
+++ frontend/src/components/User/SignUp.jsx
@@ -1,22 +1,70 @@
-import React from 'react'
-import {motion} from "framer-motion";
+import React from "react";
+import { motion } from "framer-motion";
+import "./signup.css";
 
 const SignUp = () => {
-  return <motion.div
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y:0 }}
-    transition={{ duration: 0.5 }}
-    className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden'
-  >
+  return (
+    <form style={{ border: "1px solid #ccc", padding: "20px", borderRadius: "5px" }}>
+      <div className="container_user">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        <hr className="hr_user" />
 
-  <div className='p-8'>
-    <h2
-      className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'
-    >
-      Create Account</h2>
-  </div>
+        <label htmlFor="email"><b>Email</b></label>
+        <input
+          type="text"
+          placeholder="Enter Email"
+          name="email"
+          className="input_user"
+          required
+        />
 
-  </motion.div>
-}
+        <label htmlFor="password"><b>Password</b></label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          className="input_user"
+          required
+        />
 
-export default SignUp
+        <label htmlFor="confirmPassword"><b>Repeat Password</b></label>
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="confirmPassword"
+          className="input_user"
+          required
+        />
+
+        <label>
+          <input
+            type="checkbox"
+            name="remember"
+            className="checkbox_user"
+            style={{ marginBottom: "15px" }}
+          />{" "}
+          Remember me
+        </label>
+
+        <p>
+          By creating an account you agree to our{" "}
+          <a href="#" style={{ color: "dodgerblue" }}>
+            Terms & Privacy
+          </a>.
+        </p>
+
+        <div className="clearfix_user">
+          <button type="button" className="cancelbtn_user">
+            Cancel
+          </button>
+          <button type="submit" className="signupbtn_user">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default SignUp;
