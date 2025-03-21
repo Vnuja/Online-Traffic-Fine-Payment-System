64d9756be09ce294e9791234d3c99de7bb084f6f
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2025-03-21 22:02:00 +0530

    Add FloatingShape component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import FloatingShape from "./components/User Tools/FloatingShape";
 
 //User Management System
 import SignUp from "./components/User/SignUp";
@@ -7,7 +8,8 @@ import Login from "./components/User/Login";
 function App() {
 
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br from-gray-700 via-gray-800 to-gray-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape />
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
