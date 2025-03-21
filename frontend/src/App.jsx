87e5416bcd82fd5f3569284d7cc14506dd2f00cd
Haritha Sandaruwan <155653525+Harithasandaruwan@users.noreply.github.com>
2025-03-21 23:52:09 +0530

    Update styling in frontend components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Login from "./components/User/Login";
 function App() {
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-700 via-gray-800 to-gray-900 flex items-center justify-center relative overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-b from-purple-800 via-purple-600 to-purple-300 flex items-center justify-center relative overflow-hidden">
       <FloatingShape />
       <Routes>
         <Route path="/signup" element={<SignUp />} />
--- frontend/src/components/User/SignUp.jsx
+++ frontend/src/components/User/SignUp.jsx
@@ -33,7 +33,7 @@ const SignUp = () => {
 			overflow-hidden'
 		>
 			<div className='p-8'>
-				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
+				<h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 via-[#C68EFD] to-emerald-500 text-transparent bg-clip-text'>
 					Create Account
 				</h2>
 
@@ -63,9 +63,9 @@ const SignUp = () => {
 					<PasswordStrengthMeter password={password} />
 
 					<motion.button
-						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-						font-bold rounded-lg shadow-lg hover:from-green-600
-						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						className='mt-5 w-full py-3 px-4 bg-[#C68EFD] text-white 
+						font-bold rounded-lg shadow-lg hover:bg-[#B07CE5] 
+						focus:outline-none focus:ring-2 focus:ring-[#C68EFD] focus:ring-offset-2
 						 focus:ring-offset-gray-900 transition duration-200'
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
@@ -79,7 +79,7 @@ const SignUp = () => {
 			<div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
 				<p className='text-sm text-gray-400'>
 					Already have an account?{" "}
-					<Link to={"/login"} className='text-green-400 hover:underline'>
+					<Link to={"/login"} className='text-[#C68EFD] hover:underline'>
 						Login
 					</Link>
 				</p>
